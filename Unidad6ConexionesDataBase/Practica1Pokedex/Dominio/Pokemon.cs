845ab9fdd4a7ba1dc61bb3c37cf0283f09c64c84
SebastianEscurra <sebastian_escurra@hotmail.com>
2023-06-05 23:54:02 -0300

    uso annotations para cambiar los nombre de las columnas

--- Unidad6ConexionesDataBase/Practica1Pokedex/Dominio/Pokemon.cs
+++ Unidad6ConexionesDataBase/Practica1Pokedex/Dominio/Pokemon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,15 @@ namespace Dominio
 {
     public class Pokemon
     {
+        [DisplayName("Número")]
         public int numero { get; set; }
-
+        [DisplayName("Nombre")]
         public string nombre { get; set; }
-        
+        [DisplayName("Descripción")]
         public string descripcion { get; set; }
-
+        [DisplayName("Tipo")]
         public Elemento tipo { get; set; }
+        [DisplayName("Debilidad")]
         public Elemento debilidad { get; set; }
         public string urlImagen { get; set; }
     }
